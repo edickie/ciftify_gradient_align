4727e15f3942260a7f73c8c146c57152a7606312
Erin Dickie <erin.w.dickie@gmail.com>
2020-04-04 16:05:50 -0400

    modifying for erin's env

--- code/01.1_submit_qbatch_dtseries_to_gradients.sh
+++ code/01.1_submit_qbatch_dtseries_to_gradients.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-qbatch -b slurm -w 00:40:00 qbatch__dtseries_to_gradients.txt -c 8 -j 4 
+# qbatch -b slurm -w 00:40:00 qbatch__dtseries_to_gradients.txt -c 8 -j 4
+
+module load gnu-parallel qbatch/2.1.5
+
+cat ${HOME}/code/chitah/subject_lists/HCP_unrelated_REST_sample?.txt | \
+ parallel echo ${HOME}/code/ciftify_gradient_align/code/01_dconn_to_gradients.sh {} |\
+ head -10 | \
+ qbatch -b slurm -w 00:40:00 -N rerun_dconn_raw_grad --env none --header "module load gnu-parallel" -c 1 -j 1 -
